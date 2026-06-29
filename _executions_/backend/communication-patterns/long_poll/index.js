000e747cc2f56430cd9c9d8c29b3321c0b0c87cd
Aniket Hans <aniketdharya@gmail.com>
2026-06-30 02:14:05 +0530

    Added comments in long polling

--- _executions_/backend/communication-patterns/long_poll/index.js
+++ _executions_/backend/communication-patterns/long_poll/index.js
@@ -1,3 +1,13 @@
+/*
+  Long polling is a technique where the client asks the server for new data, but instead of responding immediately, the server waits until it has new data (or a timeout occurs).
+
+  How it works
+  Client sends a request: "Do you have any new messages?"
+  Server keeps the request open if there's nothing new.
+  When new data arrives, the server sends the response.
+  The client immediately sends another request and the cycle repeats.
+*/
+
 import express from "express";
 
 const app = express();
@@ -12,11 +22,12 @@ app.post("/job", (req, res) => {
 });
 
 app.get("/status/:jobid", async (req, res) => {
-  while (!(await getJobStatus(req.params.jobid)));
+  while (!(await getJobStatus(req.params.jobid))); // Wait till the job is completed then it will send the response
   res.end(`Job Done with id ${req.params.jobid}`);
 });
 
 function getJobStatus(jobid) {
+  // checks the status of te job
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (jobs[jobid] < 100) {
@@ -28,6 +39,7 @@ function getJobStatus(jobid) {
 }
 
 function processJob(jobid) {
+  // processes the job async
   const TO = setTimeout(() => {
     jobs[jobid] += 10;
     console.log(`Job ${jobid} processed ${jobs[jobid]}%`);
